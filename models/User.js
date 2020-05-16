add28e4782ea98558bfa294da43967fabd04ff6b
csfinch <carl@aababot.com>
2020-05-15 20:14:29 -0400

    Update project template allowing signup/signin and authenticating users with templates

--- models/User.js
+++ models/User.js
@@ -4,30 +4,17 @@ module.exports = function(sequelize, DataTypes) {
   return sequelize.define('User', {
     id: {
       type: DataTypes.INTEGER,
+      autoIncrement: true,
       allowNull: false,
       primaryKey: true
     },
     email: {
-      type: DataTypes.STRING(255),
-      allowNull: true
+      type: DataTypes.STRING(8000),
+      allowNull: false
     },
     password: {
-      type: DataTypes.STRING(255),
+      type: DataTypes.STRING(8000),
       allowNull: false
-    },
-    createdAt: {
-      type: DataTypes.DATE,
-      allowNull: false
-    },
-    updatedAt: {
-      type: DataTypes.DATE,
-      allowNull: false
-    },
-    role: {
-      type: DataTypes.STRING(10),
-      allowNull: true,
-      default: 'basic',
-      enum: ["basic", "supervisor", "admin"]
     }
   }, {
     tableName: 'User'
