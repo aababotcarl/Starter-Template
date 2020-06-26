66d15b4a8882d157e7ef873a078a1b00d4fda817
carl <carl@localhost-live.attlocal.net>
2020-06-26 07:53:20 -0400

    Added social oauth

--- Routes/index.js
+++ Routes/index.js
@@ -1,3 +1,6 @@
+var passportFacebook = require('../auth/facebook');
+var passportGoogle = require('../auth/google');
+var passportTwitter = require('../auth/twitter');
 var homeCtrl = require('../Controllers/home');
 var accountCtrl = require('../Controllers/account');
 module.exports = function(app, passport){
@@ -8,13 +11,13 @@ module.exports = function(app, passport){
     app.get('/account/dashboard', isAuthenticated, accountCtrl.dashboard);
     app.post('/account/signin', passport.authenticate('local-signin', {successRedirect: '/account/dashboard', failureRedirect: '/account/signin'}));
     app.post('/account/signup', passport.authenticate('local-signup', {successRedirect: '/account/dashboard', failureRedirect: '/account/signup'}));
-    app.get('/account/facebook', passport.authenticate('facebook', {scope: ['public_profile', 'email']}));
-    app.get('/account/facebook/callback', passport.authenticate('facebook', {successRedirect: '/account/dashboard', failureRedirect: '/'}));
-    app.get('/account/twitter', passport.authenticate('twitter', {scope: 'email'}));
-    app.get('/account/twitter/callback', passport.authenticate('twitter', {successRedirect: '/account/dashboard', failureRedirect: '/'}));
-    app.get('/account/google', passport.authenticate('google', {scope: ['profile', 'email']}));
-    app.get('/account/google/callback', passport.authenticate('google', {successRedirect: '/account/dashboard', failureRedirect: '/'}));
-        app.get('/account/signout', accountCtrl.signout);
+    app.get('/account/facebook', passportFacebook.authenticate('facebook', {failureRedirect: '/account/signin'}));
+    app.get('/account/facebook/callback', passportFacebook.authenticate('facebook', {failureRedirect: '/account/signin'}), function(req, res){res.redirect('/account/dashboard')});
+    app.get('/account/twitter', passportTwitter.authenticate('twitter'));
+    app.get('/account/twitter/callback', passportTwitter.authenticate('twitter', {failureRedirect: '/account/signin'}), function(req, res){res.redirect('/account/dashboard')});
+    app.get('/account/google', passportGoogle.authenticate('google', {scope: ['https://www.googleapis.com/auth/plus.login']}));
+    app.get('/account/google/callback', passportGoogle.authenticate('google', {failureRedirect: '/account/singin'}), function(req, res){res.redirect('/account/dashboard')});
+    app.get('/account/signout', accountCtrl.signout);
     //check to see if user is authenticated
     function isAuthenticated(req, res, next){
         if(req.isAuthenticated()){
